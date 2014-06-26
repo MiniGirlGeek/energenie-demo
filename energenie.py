ed276eb8ce53c7a4a9cf3edd15986a60f8747da0
ghollingworth <gordon@raspberrypi.org>
2014-06-26 11:03:24 +0100

    Bit of commentry
    Removed a couple of magic numbers and added a couple of comments

--- energenie.py
+++ energenie.py
@@ -1,10 +1,14 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+# The GPIO pins for the Energenie module
 bit1 = 11
 bit2 = 15
 bit3 = 16
 bit4 = 13
+
+ook = 18
+enable = 22
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -14,17 +18,20 @@ GPIO.setup(bit2, GPIO.OUT)
 GPIO.setup(bit3, GPIO.OUT)
 GPIO.setup(bit4, GPIO.OUT)
 
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
+GPIO.setup(ook, GPIO.OUT)
+GPIO.setup(enable, GPIO.OUT)
 
-GPIO.output(22, False)
-GPIO.output(18, False)
+GPIO.output(ook, False)
+GPIO.output(enable, False)
 
 GPIO.output(bit1, False)
 GPIO.output(bit2, False)
 GPIO.output(bit3, False)
 GPIO.output(bit4, False)
 
+# Codes for switching on and off the sockets
+# the first [0] is all on / off
+all_sockets = 0
 on =  ['1011', '0111', '0110', '0101', '0100']
 off = ['0011', '1111', '1110', '1101', '1100']
 
@@ -38,9 +45,9 @@ def change_plug_state(socket, on_or_off):
     state = on_or_off[socket][-4] == '1'
     GPIO.output(bit4, state)
     sleep(0.1)
-    GPIO.output(22, True)
+    GPIO.output(enable, True)
     sleep(0.25)
-    GPIO.output(22, False)
+    GPIO.output(enable, False)
 
 while True:
     raw_input('Hit any key to turn on: ')
@@ -48,4 +55,4 @@ while True:
     change_plug_state(2, on)
     raw_input('Hit any key to turn off: ')
     print('turning off')
-    change_plug_state(0, off)
+    change_plug_state(all_sockets, off)
